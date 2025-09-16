c0a11f3da2d6b86e4d38224d5e92b7a40aa1787e
lucasbuloz <lucasbuloz@gmail.com>
2025-09-16 16:49:22 -0300

    Delete Probando1.hs

--- Probando1.hs
+++
@@ -1,8 +0,0 @@
---ghc -o main NombreDelArchivo.hs; .\main               de esta manera corre el programa
-
-rango_permitido:: (Integral a) => [a] -> [a]
-rango_permitido x = (minimum x) : ((maximum x): [])
-
-
-last1:: [a]-> a
-last1 x=head (reverse x)
